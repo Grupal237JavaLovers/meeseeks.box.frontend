757be6ed23d199251052198a4dd35c55688d16d1
ivladon <vladnoimis@gmail.com>
2017-11-19 00:37:41 +0200

    FML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mb-root',
-  template: '<mb-layout></mb-layout>',
+  template: '<mb-landing-page></mb-landing-page>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
