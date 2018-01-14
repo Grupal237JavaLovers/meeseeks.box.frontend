c7015630f49848e696c64543087ece33c3d736ae
alexandrustoica <13.01.1996ISbtQWERTY>
2018-01-14 15:09:39 +0200

    Jobs Grid Design

--- src/app/job1/jobs-grid/jobs-grid.component.ts
+++ src/app/job1/jobs-grid/jobs-grid.component.ts
@@ -1,6 +1,7 @@
 /**
  * Created by csebestin on 11/21/2017.
  */
+
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
@@ -8,6 +9,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './jobs-grid.component.html',
   styleUrls: ['./jobs-grid.component.scss'],
 })
+
 export class MbJobsGridComponent {
   @Input() jobs: any = [];
   @Output() jobClicked = new EventEmitter<any>();
