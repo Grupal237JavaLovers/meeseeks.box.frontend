bacd98c6a619d776e4afb788ec4c5349d6124c0b
Claudia Sebestin <Claudia.Sebestin@catalysts.cc>
2018-01-20 12:36:51 +0200

    fix profile page

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,33 +1,33 @@
 /**
  * Created by csebestin on 10/23/2017.
  */
-import {Component, OnInit} from '@angular/core';
-import {UserService} from '../user.service';
-import {Provider} from '../../model/provider';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { EmbedVideoService } from 'ngx-embed-video';
-import {Router} from '@angular/router';
-import {User} from '../../model/user';
-import {Consumer} from '../../model/consumer';
+import { Consumer } from '../../model/consumer';
+import { Provider } from '../../model/provider';
+import { User } from '../../model/user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'mb-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss', '../../shared/variables.scss']
+  styleUrls: ['./profile.component.scss', '../../shared/variables.scss'],
 })
 export class MbProfileComponent implements OnInit {
   user: User;
-  provider: Provider;
+  provider: Provider = new Provider({});
   consumer: Consumer;
   imageUrl: string;
   description: string;
   rating: number;
   isProvider: boolean;
   iframe_html: any;
-  constructor(
-    private userService: UserService,
-    private embedService: EmbedVideoService,
-    private router: Router
-  ) { }
+
+  constructor(private userService: UserService,
+              private embedService: EmbedVideoService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
     this.user = this.userService.getUser();
@@ -38,12 +38,13 @@ export class MbProfileComponent implements OnInit {
           this.provider = data;
           this.imageUrl = this.provider.profileImageUrl;
           this.description = this.provider.description;
-          this.iframe_html = this.embedService.embed(this.provider.profileVideoUrl, {attr: {width: 500, height: 500}});
+          if (this.provider.profileVideoUrl) {
+            this.iframe_html = this.embedService.embed(this.provider.profileVideoUrl, {attr: {width: 500, height: 500}});
+          }
 
           this.userService.getProviderSkills(this.user.id)
             .then(res => {
-              // this.provider.skills = data;
-              this.provider.skills = ['Programmer', 'Musician'];    // hardcoded
+              this.provider.skills = res;
             })
             .catch(err => {
               console.error(err);
@@ -51,8 +52,7 @@ export class MbProfileComponent implements OnInit {
 
           this.userService.getProviderRating(this.user.id)
             .then(res => {
-              // this.rating = res;
-              this.rating = 3.5;     // hardcoded
+              this.rating = res;
             })
             .catch(err => {
               console.error(err);
@@ -70,8 +70,7 @@ export class MbProfileComponent implements OnInit {
           this.imageUrl = this.consumer.profileImageUrl;
           this.userService.getConsumerRating(this.user.id)
             .then(res => {
-              // this.rating = res;
-              this.rating = 3.5;     // hardcoded
+              this.rating = res;
             })
             .catch(err => {
               console.error(err);
